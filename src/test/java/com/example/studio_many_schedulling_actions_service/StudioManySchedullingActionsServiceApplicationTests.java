ef8f7b60904ec085871a4fecdc19b19c8b613f99
Rafael-Mechi <rafael.doliveira@sptech.school>
2026-08-29 18:05:11 -0300

    adicionando dependencias

---
+++ src/test/java/com/example/studio_many_schedulling_actions_service/StudioManySchedullingActionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studio_many_schedulling_actions_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioManySchedullingActionsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
